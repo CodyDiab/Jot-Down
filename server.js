711dc3c5ee060b5d44cf5cbf2b11c0935302fcd3
Diab <emailcodydiab@gmail.com>
2020-05-29 07:53:47 -0500

    add html routes to server

--- server.js
+++ server.js
@@ -103,6 +103,17 @@ app.delete("/api/notes/:id", (req,res) => {
 });
 
 ///Routes here?
+app.get("notes",(req,res) => {
+    res.sendFile(path.join(__dirname,"/public/index.html"));
+});
+
+app.get("/", (req,res) => {
+    res.JSON(path.join(__dirname,"public/index.html"));
+});
+
+app.get("*", (reg,res) => {
+    res.sendFile(path.join(__dirname,"/public/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening on PORT:${PORT}`)
